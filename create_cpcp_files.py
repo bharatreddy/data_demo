9a89764d23debbd836f946f310193bd9c490000b
bharatreddy <bharatr@vt.edu>
2018-09-10 16:26:51 -0400

    generate cpcp files

---
+++ create_cpcp_files.py
@@ -0,0 +1,39 @@
+import pandas
+import datetime
+import glob
+import bz2
+import time
+
+# set up some input params
+srchDir = "/sd-data/2015/mappot_ascii/north/"
+srchFilePtrn = "2015010*.bz2"
+cpcpSrchString = "> Potential:"
+dtSrchString = "d "
+# Loop over the files and collect datetime/cpcp info
+# for each 2 minute time interval!
+# arrays to store the data
+dtList = []
+cpcpList = []
+for _fName in glob.glob(srchDir + srchFilePtrn):
+    # open the file
+    print "working with---->", _fName
+    _file = bz2.BZ2File(_fName)
+    for _line in _file:
+        if _line.startswith(dtSrchString):
+            dtStr = _line.split("d ")[1].strip("\n")
+            dtObj = datetime.datetime.strptime(dtStr,"%Y-%m-%d/%H:%M:%S")
+            dtList.append( dtObj )
+        if cpcpSrchString in _line:
+            cpStr = _line.split("Drop=")[1]
+            cpVal = cpStr.split(" kV")
+            cpcpList.append( int(cpVal[0]) )
+
+# create a dataframe from the data
+cpcpDF = pandas.DataFrame({
+    "date" : dtList,
+    "cpcp" : cpcpList
+    }
+    )
+# store the dataframe in a csv file
+cpcpDF.to_csv("data/CPCP-2015.csv", index=False, columns=["date", "cpcp"])
+cpcpDF.to_feather("data/CPCP-2015.feather")
